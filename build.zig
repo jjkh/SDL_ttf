c062d2cff8b67db71c09ae84d83c703ec531485d
Jay Petacat <jay@jayschwa.net>
2024-02-02 16:59:28 -0700

    Package for Zig 0.12.0-dev.2541+894493549

---
+++ build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "SDL2_ttf",
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.addCSourceFile(.{ .file = .{ .path = "SDL_ttf.c" } });
+    lib.linkLibC();
+
+    const freetype_dep = b.dependency("freetype", .{
+        .target = target,
+        .optimize = optimize,
+        .enable_brotli = false,
+    });
+    lib.linkLibrary(freetype_dep.artifact("freetype"));
+
+    const sdl_dep = b.dependency("sdl", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    lib.linkLibrary(sdl_dep.artifact("SDL2"));
+
+    lib.installHeader("SDL_ttf.h", "SDL2/SDL_ttf.h");
+    b.installArtifact(lib);
+}
